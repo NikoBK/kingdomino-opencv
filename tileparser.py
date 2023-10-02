2e51865d4460344786cc738e1b051eb61530ec93
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-10-02 18:14:07 +0200

    terrain type detection

--- tileparser.py
+++ tileparser.py
@@ -25,6 +25,8 @@ class TileParser:
     def __init__(self):
         self.tile_size = 100 # Pixels
         self.tiles = []
+        self.font = cv.FONT_HERSHEY_SIMPLEX
+        self.font_org = (50, 50)
 
     def parse_tiles(self, img):
         color_map_matrix = np.zeros([5,5,3], dtype=np.uint8)
@@ -38,7 +40,10 @@ class TileParser:
                 avg_color_row = np.average(tile.img, axis=0)
                 avg_color = np.average(avg_color_row, axis=0)
                 tile.average_color = avg_color
-
+                test = self.parse_terrain(tile)
+                
+                print(f"tile ({tile.x},{tile.y})'s average color is: {tile.average_color}")
+                print(f"tile ({tile.x},{tile.y})'s terrain is: {test}")
                 # Only used for debugging.
                 color_map_matrix[tile.y, tile.x] = [
                     tile.average_color[0], # Red
@@ -46,6 +51,24 @@ class TileParser:
                     tile.average_color[2]  # Blue
                 ]
         return color_map_matrix
+    
+    def parse_terrain(self, tile):
+        blue = int(tile.average_color[0])
+        green = int(tile.average_color[1])
+        red = int(tile.average_color[2])
+
+        # print(f"red: {red}, green: {green}, blue: {blue}")
+
+        # Check tile colors
+        if blue > 18 and green > 139 and red > 96:
+        # if red > 28 and green > 148 and blue > 96:
+            return "plains"
+        elif blue > 120 and green > 73 and red < 56:
+            return "ocean"
+        elif blue < 40 and green < 70 and red < 57:
+            return "forest"
+        else:
+            return "None"
 
 
 
